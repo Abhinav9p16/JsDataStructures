aceb2bf11d40f6f0c8eba5467d41bd6389bf2ed6
Abhinav9p16 <thakurabhinav56@gmail.com>
2020-11-10 22:06:33 +0530

    Added: Code to find unique triplets with sum zero in array

--- Programs.js
+++ Programs.js
@@ -1,23 +1,23 @@
-const prompt = require('prompt-sync')();
-
-let num1 = prompt("Enter the starting value of the range 0-100:  ")
-let num2 = prompt("Enter the ending value of the range 0-100:  ")
-
-num1 = parseInt(num1)
-num2 = parseInt(num2)
-let numArray = new Array()
-
-if(num1 > num2){
-    console.log("Invalid Range")
-    return
-}
-else{
-    for(let i = num1; i <= num2; i++){
-        let temp = i.toString();
-        temp = temp.split("")
-        if(temp[0] == temp[1])
-            numArray.push(i)
+function findTriplets(arr, n) {
+    arr.sort();
+    var l = arr.length;
+    for (var i = 0; i < l; i++) {
+        var j = i + 1,
+            k = l - 1;
+        while (j < k) {
+            if (arr[i] + arr[j] + arr[k] < n) {
+                j++;
+            } else if (arr[i] + arr[j] + arr[k] > n) {
+                k--;
+            } else {
+                console.log(arr[i] + "," + arr[j] + "," + arr[k]);
+                j++;
+                k--;
+            }
+        }
     }
+    return true;
 }
 
-console.log("Numbers with repeated digits are:  " + numArray.toString())
+var arr = [-1, -4, -9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+findTriplets(arr, 0);
